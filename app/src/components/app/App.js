2da34bbc8ab2fcc9aab7e3c4a45979eb200ef954
Alejandro Bravo <abravo16@stanford.edu>
2019-02-09 21:54:08 -0800

    add toolbar buttons with material-ui

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Toolbar from '../toolbar/Toolbar';
 import Canvas from '../canvas/Canvas';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <div className="main">
 
         </div>
+        <Toolbar />
       </Fragment>
     );
   }
--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,7 @@ import './css/index.css';
 import App from './components/app/App';
 import registerServiceWorker from './js/registerServiceWorker';
 import epiphany_canvas from './js/epiphany_canvas';
+// import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
