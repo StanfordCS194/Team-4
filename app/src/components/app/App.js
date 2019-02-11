36b55fd6ea49f6127b26390a2456410e3d0193b6
seankdecker <seankdecker@gmail.com>
2019-02-10 17:03:16 -0800

    added content to sidebar

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -6,11 +6,18 @@ import Canvas from '../canvas/Canvas';
 import FloatingToolbar from '../floatingToolbar/FloatingToolbar'
 import Sidebar from 'react-sidebar';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import AccountIcon from '@material-ui/icons/AccountCircle';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebarOpen: true
+      sidebarOpen: true,
+      user: {name: 'Marilu Bravo', img: '/anon.png'}
     };
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
   }
@@ -19,15 +26,35 @@ class App extends Component {
     this.setState({ sidebarOpen: open });
   }
 
+  makeSideBarContent(user) {
+    return (
+      <Fragment>
+        <div className="sidebarContent" id="user">
+          <AccountIcon id="accountIcon"/>
+          <a>{user.name}</a>
+        </div>
+        <div className="sidebarContent">
+          <a href='#'>My Boards</a>
+        </div>
+        <div className="sidebarContent">
+          <a href='#'>Account Settings</a>
+        </div>
+        <div className="sidebarContent">
+          <a href='#'>Logout</a>
+        </div>
+      </Fragment>
+    );
+  }
+
   render() {
     return (
       <Fragment>
         <h3 style={{ textAlign: 'center' }}>epiphany</h3>
         <Sidebar
-          sidebar={<b>Sidebar content</b>}
+          sidebar={this.makeSideBarContent(this.state.user)}
           open={this.state.sidebarOpen}
           onSetOpen={this.onSetSidebarOpen}
-          styles={{ sidebar: { background: "white" } }}
+          styles={{ sidebar: { background: "white", textAlign: "center", padding: "10px", backgroundColor: "#3f51b5"} }}
         >
           <div id="handle" onMouseEnter={() => this.onSetSidebarOpen(true)}>
           >
