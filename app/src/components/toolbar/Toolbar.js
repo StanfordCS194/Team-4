436a70aa6375cc8d1cb70518cd7f1f7c776d2cce
Carmelle Millar <carmelle@stanford.edu>
2019-02-13 14:58:11 -0800

    lil icon added to the toolbar

--- app/src/components/toolbar/Toolbar.js
+++ app/src/components/toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { ButtonToolbar } from 'react-bootstrap';
 import './Toolbar.css';
+import { Button } from 'react-native'
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -10,6 +11,8 @@ import Icon from '@material-ui/core/Icon';
 import DeleteIcon from '@material-ui/icons/Delete';
 import SaveIcon from '@material-ui/icons/Save';
 import UndoIcon from '@material-ui/icons/Undo';
+import ColorIcon from '@material-ui/icons/FormatColorFill';
+
 
 class Toolbar extends Component {
   constructor(props) {
@@ -22,10 +25,17 @@ class Toolbar extends Component {
   createToolDivs() {
     return (
       <Fragment>
-      <Fab id={this.state.tools[0]} color="primary" className={this.state.tools[0]}>
+      <Fab 
+        id={this.state.tools[0]} 
+        color="primary" 
+        className={this.state.tools[0]}
+        onClick={() => console.log('Undo')}
+        >
           <UndoIcon /></Fab>
       <Fab id={this.state.tools[1]} color="primary" className={this.state.tools[1]}>
           <SaveIcon /></Fab>
+        <Fab id={this.state.tools[2]} color="primary" className={this.state.tools[2]}>
+          <ColorIcon /></Fab>
       <Fab id={this.state.tools[2]} color="primary" className={this.state.tools[2]}>
           <DeleteIcon /></Fab>
       </Fragment>
