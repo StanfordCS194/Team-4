557ad8a67356982f2c8700b7489e5979f09ac7b4
marilubravo <mkbravo@stanford.edu>
2019-02-19 16:53:03 -0800

    Modify index.js and package.json

--- app/index.js
+++ app/index.js
@@ -9,3 +9,11 @@ import epiphany_canvas from './js/epiphany_canvas';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 epiphany_canvas();
+
+
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
