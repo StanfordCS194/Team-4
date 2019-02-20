907f53713df8e3b5684523bdb26eeec842279598
marilubravo <mkbravo@stanford.edu>
2019-02-20 12:00:23 -0800

    Setup PM2

---
+++ app/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'epiphany',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-188-126-24.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/EpiphanyKeyPair.pem',
+      ref: 'origin/mb_backend',
+      repo: 'git@github.com:StanfordCS194/epiphany.git',
+      path: '/home/ubuntu/epiphany',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
