393ea198d6955be710ffa4c3ac46d2ebbfc48338
marilubravo <mkbravo@stanford.edu>
2019-02-20 13:56:57 -0800

    Node doesn't understand import

--- app/index.js
+++ app/index.js
@@ -1,16 +1,22 @@
 //import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/app/App';
-import registerServiceWorker from './js/registerServiceWorker';
-import epiphany_canvas from './js/epiphany_canvas';
+//import ReactDOM from 'react-dom';
+//import './css/index.css';
+//import App from './components/app/App';
+//import registerServiceWorker from './js/registerServiceWorker';
+//import epiphany_canvas from './js/epiphany_canvas';
 // import bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
+
+const ReactDom require('react-dom');
+const registerServiceWorker require ('./js/registerServiceWorker');
+const epiphany_canvas require ('./js/epiphany_canvas')
+const App require ('./components/app/App')
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 epiphany_canvas();
 
-//
+
 const express = require('express')
 const app = express()
 
