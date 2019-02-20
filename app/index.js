bed6c8453ccf9bcaf11dc41f8be349ba82b90dea
marilubravo <mkbravo@stanford.edu>
2019-02-20 13:45:13 -0800

    Uncomment our index.js code

--- app/index.js
+++ app/index.js
@@ -1,16 +1,16 @@
 //import React from 'react';
-//import ReactDOM from 'react-dom';
-//import './css/index.css';
-//import App from './components/app/App';
-//import registerServiceWorker from './js/registerServiceWorker';
-//import epiphany_canvas from './js/epiphany_canvas';
-//// import bootstrap from 'bootstrap/dist/css/bootstrap.css';
-//
-//ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
-//epiphany_canvas();
-//
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './components/app/App';
+import registerServiceWorker from './js/registerServiceWorker';
+import epiphany_canvas from './js/epiphany_canvas';
+// import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+epiphany_canvas();
+
+//
 const express = require('express')
 const app = express()
 
