0d431a5dad65b96bad19ffe3a5315f71b9546006
seankdecker <seankdecker@gmail.com>
2019-03-02 00:16:30 -0800

    I have made it so that all react stuff is compiled and put into folder called compiled, which is then run in index.html

--- app/server.js
+++ app/server.js
@@ -1,17 +1,23 @@
 'use strict';
 
+// build app using express
 var express = require('express');
 var app = express();
 
-// We have the express static module (http://expressjs.com/en/starter/static-files.html) do all
-// the work for us.
+// add favicon
+var favicon = require('serve-favicon');
+app.use(favicon(__dirname + '/src/media/favicon.ico'));
+
+// use the directory that this file is in as the main directory
 app.use(express.static(__dirname));
 
-
+// simple response to the home directory of wherever this is served
+// send response and load index.html
 app.get('/', function (request, response) {
     response.send('Simple web server of files from ' + __dirname);
 });
 
+// have app listen on port 3000
 var server = app.listen(3000, () => {
   var port = server.address().port;
   console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
