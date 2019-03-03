3c08da8fd9e442dad36cf1e736f03bf449c8835e
Alejandro Bravo <abravo16@stanford.edu>
2019-03-02 20:11:32 -0800

    fix undo button bug

--- app/src/components/toolbar/Toolbar.js
+++ app/src/components/toolbar/Toolbar.js
@@ -1,11 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import React, {Component, Fragment} from 'react';
+import {Button, ButtonToolbar} from 'react-bootstrap';
 import './Toolbar.css';
 
 import ColorPicker from '../colorpicker/ColorPicker';
 
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Icon from '@material-ui/core/Icon';
@@ -15,64 +15,63 @@ import UndoIcon from '@material-ui/icons/Undo';
 import ColorFillIcon from '@material-ui/icons/FormatColorFill';
 
 
-
 class Toolbar extends Component {
-  constructor(props) {
-    console.log(props);
-    super(props);
-    this.state = {
-      tools: ['undo', 'save', 'delete', 'color'],
-      openColorPicker: false,
-      icons: [<UndoIcon />, <SaveIcon />, <DeleteIcon />, null]
-    };
-  }
+    constructor(props) {
+        console.log(props);
+        super(props);
+        this.state = {
+            tools: ['undo', 'save', 'delete', 'color'],
+            openColorPicker: false,
+            icons: [<UndoIcon onClick={this.props.undo}/>, <SaveIcon/>, <DeleteIcon/>, null]
+        };
+    }
 
     componentWillMount() {
         // add event listener for clicks
         document.addEventListener('click', this.handleClick, false);
     }
 
-  createToolIcons() {
-    return (this.state.icons.map((icon, i) => {
-      return <Fab
-                  id={this.state.tools[i]}
-                  key={this.state.tools[i]}
-                  color="primary"
-                  className="tools"
-                  >
-                  {this.state.icons[i]}
-                  {this.state.tools[i] === 'color' &&
-                    <ColorPicker
-                        openColorPicker={this.state.openColorPicker}
-                        onColorChange={this.props.onColorChange}
-                    />
-                  }
-                  {this.state.tools[i] === 'color' &&
-                  <span id="colorDot" style={{backgroundColor: this.props.nextColor}}/>}
-              </Fab>
+    createToolIcons() {
+        return (this.state.icons.map((icon, i) => {
+            return <Fab
+                id={this.state.tools[i]}
+                key={this.state.tools[i]}
+                color="primary"
+                className="tools"
+            >
+                {this.state.icons[i]}
+                {this.state.tools[i] === 'color' &&
+                <ColorPicker
+                    openColorPicker={this.state.openColorPicker}
+                    onColorChange={this.props.onColorChange}
+                />
+                }
+                {this.state.tools[i] === 'color' &&
+                <span id="colorDot" style={{backgroundColor: this.props.nextColor}}/>}
+            </Fab>
 
-    }))
-  }
+        }))
+    }
 
-  handleClick = e => {
-      if (e.target.id !== 'color' && e.target.id !== 'colorDot') {
-          this.setState({ openColorPicker: false });
-      } else {
-          this.setState({ openColorPicker: !this.state.openColorPicker });
-      }
+    handleClick = e => {
+        if (e.target.id !== 'color' && e.target.id !== 'colorDot') {
+            this.setState({openColorPicker: false});
+        } else {
+            this.setState({openColorPicker: !this.state.openColorPicker});
+        }
 
-      if (e.target.id === 'undo') {
-        this.props.undo();
-      }
-  };
+        if (e.target.id === 'undo') {
+            this.props.undo();
+        }
+    };
 
-  render() {
-      return (
-          <ButtonToolbar className="toolbar">
-            {this.createToolIcons()}
-          </ButtonToolbar>
-    );
-  }
+    render() {
+        return (
+            <ButtonToolbar className="toolbar">
+                {this.createToolIcons()}
+            </ButtonToolbar>
+        );
+    }
 }
 
 export default Toolbar;
